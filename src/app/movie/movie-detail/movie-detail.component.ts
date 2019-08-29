82353c5387282f8a5a633c8cbf5f7a1356498457
Sakis Manal <manowarlik@gmail.com>
2019-08-30 00:43:54 +0300

    finish movie-detail comp

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -5,7 +5,7 @@ import { Movie } from '../movie';
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.css']
+  styleUrls: ['./movie-detail.component.scss']
 })
 export class MovieDetailComponent implements OnInit {
 
